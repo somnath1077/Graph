78c697af896007e2f2999a294dd839da8652c30d
Somnath Sikdar <sikdar@cs.rwth-aachen.de>
2014-09-22 16:27:08 +0200

    added comments to Edge class

--- SimpleGraph/src/simplegraph/Edge.java
+++ SimpleGraph/src/simplegraph/Edge.java
@@ -1,20 +1,33 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package simplegraph;
 
 /**
- *
+ *  
  * @author sikdar
+ * An edge is viewed is an ordered pair of vertices. In directed graphs, the pair 
+ * is stored as (tail, head). In undirected graphs, the pair is stored as: 
+ * (vertex_with_smaller_index, vertex_with_larger_index).
  */
 public interface Edge {
 
+    /**
+     * Takes a vertex and returns the other endpoint/ 
+     * @param v
+     * @return the other endpoint of the edge
+     */
     int getOther(int v);
 
+    /**
+     *  if undirected, this gives the smaller of the two endpoints;
+     *  if directed, this gives tail.
+     * @return 
+     */
     int getV1();
 
+    /**
+     * if undirected, this gives the larger of the two endpoints 
+     * if directed, this gives head
+     * @return 
+     */
     int getV2();
     
 }
