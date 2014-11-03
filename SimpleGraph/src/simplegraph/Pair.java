d594ab2035001e21de88b685eb0da3e83a937489
sikdar <sikdar@cs.rwth-aachen.de>
2014-11-03 14:17:10 +0100

    added a Pair class to work with FlowInspector

---
+++ SimpleGraph/src/simplegraph/Pair.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package simplegraph;
+
+/**
+ *
+ * @author somnath
+ */
+public class Pair<A, B> {
+
+    private A first;
+    private B second;
+
+    public Pair(A first, B second) {
+        super();
+        this.first = first;
+        this.second = second;
+    }
+
+    public A getFirst() {
+        return first;
+    }
+
+    public void setFirst(A first) {
+        this.first = first;
+    }
+
+    public B getSecond() {
+        return second;
+    }
+
+    public void setSecond(B second) {
+        this.second = second;
+    }
+
+    public int hashCode() {
+        int hashFirst = first != null ? first.hashCode() : 0;
+        int hashSecond = second != null ? second.hashCode() : 0;
+
+        return (hashFirst + hashSecond) * hashSecond + hashFirst;
+    }
+
+    public boolean equals(Object obj) {
+        if (!(obj instanceof Pair)) {
+            return false;
+        }
+
+        Pair other = (Pair) obj;
+        if (this.first == other.first
+                || (this.first != null && other.first != null && this.first.equals(other.first))
+                && this.second == other.second
+                || (this.second != null && other.second != null && this.second.equals(other.second))) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public String toString() {
+        return "(" + first.toString() + ", " + second.toString() + ")";
+    }
+}
